e87f78122adaed719583a096eff1d36de3e42ea6
Jakub Perich <j.perich@seznam.cz>
2019-05-22 15:11:03 +0200

    Loads saved data from Core Data

--- ToDoList/ListViewController.swift
+++ ToDoList/ListViewController.swift
@@ -7,14 +7,118 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UITableViewController {
-
+    
+    var items = [Tasks]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        loadItems()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    //MARK: TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.completed ? .checkmark : .none
+        if item.completed {
+            cell.backgroundColor = .green
+        } else {
+            cell.backgroundColor = .white
+        }        
+        return cell
+        
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let newItem = Tasks(context: self.context)
+            
+            
+            newItem.title = textField.text!
+            
+            self.items.insert(newItem, at: 0)
+            
+            self.saveTask()
+            
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a New Item"
+        }
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func saveTask() {
+        do{
+            try context.save()
+        }catch {
+            print("Error saving item with \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        items[indexPath.row].completed = !items[indexPath.row].completed
+        saveTask()
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if (editingStyle == .delete) {
+            
+            let item = items[indexPath.row]
+            items.remove(at: indexPath.row)
+            context.delete(item)
+            
+            do {
+                try context.save()
+            } catch {
+                print("Error deleting items with \(error)")
+            }
+            tableView.deleteRows(at: [indexPath], with: .automatic) 
+        }
+    }
+    
+    func loadTasks(){
+        let request: NSFetchRequest<Tasks> = Tasks.fetchRequest()
+        
+        do {
+            items = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    
 }
-
