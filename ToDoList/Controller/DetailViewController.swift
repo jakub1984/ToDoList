c25cae45f5c5558892fb60c3df24c3c08da4b076
Jakub Perich <j.perich@seznam.cz>
2019-05-23 17:57:12 +0200

    Category picker

--- ToDoList/Controller/DetailViewController.swift
+++ ToDoList/Controller/DetailViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import CoreData
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    
     
     @IBOutlet weak var txtDatePicker: UITextField!
     let datePicker = UIDatePicker()
-
+    let pickerView = UIPickerView()
+    @IBOutlet weak var pickerTextField: UITextField!
+    
     
     var selectedTask: Tasks?{
         didSet{
@@ -24,18 +28,23 @@ class DetailViewController: UIViewController {
         }
     }
     
-    let categoryData = [
-        [#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1), #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)],
-        ["Swift","Project","Priority","Homework","Non-work"]
-    ]
-    var managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+//    let colors = ["blue", "black", "green", "brown", "red"]
+    let pickColors = [#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1), #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)]
+    let pickCategories = ["Swift","Project","Priority","Homework","Non-work"]
+    
+    var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
-    var blue = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerView.delegate = self
+        showCategoryPicker()
         showDatePicker()
         // Do any additional setup after loading the view.
     }
+    
+    
     
     func showDatePicker(){
         //Formate Date
@@ -55,6 +64,7 @@ class DetailViewController: UIViewController {
         txtDatePicker.inputView = datePicker
         
     }
+
     
     @objc func donedatePicker(){
         let formatter = DateFormatter()
@@ -66,10 +76,63 @@ class DetailViewController: UIViewController {
     @objc func cancelDatePicker(){
         self.view.endEditing(true)
     }
+    
+    @objc func cancelCategoryPicker(){
+        self.view.endEditing(true)
+    }
 
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
         selectedTask?.dueDate = datePicker.date
+//        selectedTask?.category =
+    }
+    
+    func showCategoryPicker() {
+        
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(cancelCategoryPicker));
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        
+        toolbar.setItems([spaceButton,doneButton], animated: false)
+        
+        pickerTextField.inputAccessoryView = toolbar
+        pickerTextField.inputView = pickerView
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickCategories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let category = pickCategories[row]
+        pickerTextField.text = "\(category)"
+        pickerTextField.backgroundColor = pickColors[row]
 
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickCategories.count
+
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var pickerLabel = view as! UILabel?
+        if view == nil {  //if no label there yet
+            pickerLabel = UILabel()
+            //color the label's background
+//            let hue = CGFloat(row)/CGFloat(pickerData.count)
+            pickerLabel!.backgroundColor = self.pickColors[row]
+        }
+        let titleData = pickCategories[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSAttributedString.Key.font:UIFont(name: "Helvetica", size: 18.0)!,NSAttributedString.Key.foregroundColor:UIColor.black])
+        pickerLabel!.attributedText = myTitle
+        pickerLabel!.textAlignment = .center
+        return pickerLabel!
     }
     /*
     // MARK: - Navigation
